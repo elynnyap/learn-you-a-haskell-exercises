7ed9402ef41026e177038989b9c9b92e5b838bd8
E-Lynn Yap <yapelynn@gmail.com>
2017-03-05 23:32:20 -0600

    Implemented functor, monoid and applicative

--- 11-functors-applicative-functors-and-monoids.hs
+++ 11-functors-applicative-functors-and-monoids.hs
@@ -5,14 +5,27 @@ import Data.Monoid
 data List a = Empty | Value a (List a) deriving (Show)
 
 -- Make the list a Functor
+instance Functor List where
+  fmap f Empty = Empty
+  fmap f (Value v xs) = Value (f v) (fmap f xs)
 
 -- Write a function which appends one list on to another
-combineLists:: List a -> List a -> List a
-combineLists a b = undefined
+combineLists :: List a -> List a -> List a
+combineLists Empty xs = xs
+combineLists (Value v Empty) xs = Value v xs
+combineLists (Value v xs) ys = Value v (combineLists xs ys)
 
 -- Make our list a Monoid
+instance Monoid (List a) where
+  mempty = Empty
+  mappend = combineLists
 
 -- Make our list an Applicative
+instance Applicative List where
+  pure a = Value a Empty
+  Empty <*> _ = Empty
+  _ <*> Empty = Empty
+  (Value f xs) <*> ys = combineLists (fmap f ys) (xs <*> ys)
 
 -- Make sure that the List obeys the laws for Applicative and Monoid
 
